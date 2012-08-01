96b642f13cb96317a02175fc1d9fad3c516759ae
yagi815 <yagi815@gmail.com>
2012-08-01 11:40:44 +0900

    test

---
+++ cloudsimulator/src/testClass.java
@@ -0,0 +1,15 @@
+
+public class testClass {
+
+	/**
+	 * Desc :
+	 * @Method Name : main
+	 * @param args
+	 * 
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
